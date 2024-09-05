435ccef4a3e85ea654598eb12835a3773ad42ed1
m-triple-m <triplem656@gmail.com>
2024-09-05 13:30:50 +0530

    added ad banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,15 @@
+import AdBanner from '@/components/AdBanner';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <AdBanner
+        data-ad-slot="7814133843"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
   )
 }
 
---
+++ src/components/AdBanner.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const AdBanner = (props) => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle adbanner-customize"
+      style={{
+        display: 'block',
+        overflow: 'hidden',
+      }}
+      data-ad-client={process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
+      {...props}
+    />
+  );
+};
+
+export default AdBanner;
