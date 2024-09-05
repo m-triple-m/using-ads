aca11764b86d1edeb6f86ba6ddc5e5d3f0a85017
m-triple-m <triplem656@gmail.com>
2024-09-05 13:18:33 +0530

    added adsense script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+          crossorigin="anonymous"></Script>
+      </body>
     </html>
   );
 }
---
+++ src/app/template.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Template = ({children}) => {
+
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default Template;
